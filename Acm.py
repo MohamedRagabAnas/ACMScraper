4654c72ffe6cc350567df3b31b56eccafe7ec555
MohamedRagabAnas <39013653+MohamedRagabAnas@users.noreply.github.com>
2018-07-29 13:59:04 +0200

    Update Acm.py

--- Acm.py
+++ Acm.py
@@ -6,29 +6,39 @@ import re
 import sys
 import csv
 #options
-STRINGS_FOR_TEST = "Sherif Sakr"
 DBLP_BASE_URL = 'https://dl.acm.org/'
 PUB_SEARCH_URL = DBLP_BASE_URL + "results.cfm?within=owners.owner%3DGUIDE&srt=_score&query=persons.authors.personName:"
-# ?within=owners.owner%3DGUIDE&srt=_score&query=persons.authors.personName:
 
-def query_Acm(authorName=STRINGS_FOR_TEST):
-    driver = webdriver.Chrome()
-    driver.get(PUB_SEARCH_URL+""+authorName)
-    #html = driver.page_source
-    link = driver.find_element_by_link_text(authorName)
-    link.click()
+def readAuthorscsv(CSVFile):
+    df=pd.read_csv(CSVFile)
+    return df
 
-    affHistElems=driver.find_elements_by_xpath("/html/body/div[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr/td/div/a")
-    affHist=[]
-    for aff in affHistElems:
-        affHist.append(aff.text)
-    return affHist    
-    #return BeautifulSoup(html,"lxml")
+def query_Acm(authorNamesFile="Authors.csv"):
+
+    authorsDF=readAuthorscsv(authorNamesFile)
+    Afflst = []
+    authorNames=authorsDF['Name'].tolist()
+    
+    for authName in authorNames:
+        driver = webdriver.PhantomJS() # wen need to check Phantom js which is hidden and may be faster...
+        driver.get(PUB_SEARCH_URL+""+authName)
+        link = driver.find_element_by_link_text(authName)
+        link.click()
+
+        affHistElems=driver.find_elements_by_xpath("/html/body/div[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr/td/div/a")
+        affHist=[]
+        for aff in affHistElems:
+            affHist.append(aff.text)
+        Afflst.append(affHist)
+        
+    return authorNames,Afflst    
 
 
 def main():
-    affs=query_Acm("Robert Isele")
-    print affs
+    names,affs=query_Acm()
+    myDF=pd.DataFrame({"Names":names,"Affiliations":affs})
+    myDF.to_csv('Auhors_Affs.csv', index=False)
+    print myDF
     
 if __name__ == '__main__':
         main()
